93bb557c47b154618ff6ac4644fd169e589fc94f
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-05-08 10:29:38 +0900

    feat(AnswersList):add answersList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import defaultDataset from './dataset';
+import { AnswersList } from './components';
+import './assets/styles/App.css';
 
-const App = () => {
-  return <div>やっっほー</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: 'init',
+      dataset: defaultDataset,
+      open: false,
+    };
+  }
+
+  render() {
+    return (
+      <section className="c-section">
+        <div className="c-box">
+          <AnswersList />
+        </div>
+      </section>
+    );
+  }
+}
 
 export default App;
