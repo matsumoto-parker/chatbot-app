b4e58ff26c31d7202dce8e8a1e7f15f98da37c54
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-05-08 16:19:58 +0900

    feat(App.js): add link

--- src/App.js
+++ src/App.js
@@ -34,7 +34,15 @@ class App extends React.Component {
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch (true) {
       case nextQuestionId === 'init':
-        this.displayNextQuestion(nextQuestionId);
+        setTimeout(() => {
+          this.displayNextQuestion(nextQuestionId);
+        }, 500);
+        break;
+      case /^https:*/.test(nextQuestionId):
+        const a = document.createElement('a');
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
         break;
       default:
         const chats = this.state.chats;
@@ -45,7 +53,9 @@ class App extends React.Component {
 
         this.setState({ chats: chats });
 
-        this.displayNextQuestion(nextQuestionId);
+        setTimeout(() => {
+          this.displayNextQuestion(nextQuestionId);
+        }, 1000);
         break;
     }
   };
@@ -53,6 +63,13 @@ class App extends React.Component {
   componentDidMount() {
     const initAnswer = '';
     this.selectAnswer(initAnswer, this.state.currentId);
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area');
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight;
+    }
   }
 
   render() {
